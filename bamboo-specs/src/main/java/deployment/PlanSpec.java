3881ae8f387996dd95e9369218c63dab0288cb69
EMEA\pcastanoigle <pcastanoigle@dxc.com>
2018-10-28 11:16:31 +0100

    no message

--- bamboo-specs/src/main/java/deployment/PlanSpec.java
+++ bamboo-specs/src/main/java/deployment/PlanSpec.java
@@ -1,7 +1,6 @@
 package deployment;
 
 import com.atlassian.bamboo.specs.api.BambooSpec;
-import com.atlassian.bamboo.specs.api.builders.BambooOid;
 import com.atlassian.bamboo.specs.api.builders.deployment.Deployment;
 import com.atlassian.bamboo.specs.api.builders.deployment.Environment;
 import com.atlassian.bamboo.specs.api.builders.deployment.ReleaseNaming;
@@ -61,8 +60,6 @@ public class PlanSpec {
 	private final static String DXC_CONTINUOUS_DEPLOYMENT_TEAM = "CEGSEC_DEVOPS_GE_CD_DEVELOPERS";
 	// Project team
 	private final static String PROJECT_TEAM = "CEGSEC_DEVOPS_GE_CD_USERS";
-	
-	private BambooOid buildPlanOID = null;
 	
 	
 	
@@ -146,8 +143,6 @@ public class PlanSpec {
         // DEPLOYMENT PLAN
         final PlanSpec deployPlanSpec = new PlanSpec();
         
-        deployPlanSpec.buildPlanOID = buildPlan.getOid();
-        
         final Deployment deployment = deployPlanSpec.createDeploymentProject();
         bambooServer.publish(deployment);
         
@@ -167,12 +162,12 @@ public class PlanSpec {
     //*** AREA TO BE MODIFIED BY PROJECT TEAM (TASKS PER EACH ENVIRONMENT ***//
     public final Deployment createDeploymentProject()
 	{
-		Deployment deployment = new Deployment(new PlanIdentifier(PROJECT_NAME, PROJECT_KEY).oid(this.buildPlanOID), DEPLOYMENT_KEY);
+		Deployment deployment = new Deployment(new PlanIdentifier(PROJECT_KEY, BUILD_PLAN_KEY), DEPLOYMENT_KEY);
 		
 		deployment.description(DEPLOYMENT_PLAN_DESC);
 		deployment.releaseNaming(new ReleaseNaming(RELEASE_NAME_FORMAT).autoIncrement(true));
 		
-		//*** MODIFY THS ***//
+		//*** MODIFY THIS ***//
 		/* TODO: Modify tasks for UAT and PROD environments */
 		deployment.environments(
 			new Environment("UAT")
